a6c7e6578f0e3a87aa0f6443f5429fe8442cb097
xabi <xabipersonape@gmail.com>
2019-11-05 18:09:27 +0100

    todo correcto

--- php/DbConfig.php
+++ php/DbConfig.php
@@ -1,5 +1,5 @@
 <?php
-$local=1; //0 para la nube
+$local=0; //0 para la nube
 if ($local==1){
     $server="localhost";
     $user="root";
@@ -8,8 +8,8 @@ if ($local==1){
 }
 else{
     $server="localhost";
-    $user="id10866050_gorokotkd";
-    $pass="";
-    $basededatos="id10866050_quiz";
+    $user="id11097793_quiz";
+    $pass="123456";
+    $basededatos="id11097793_quiz";
 }
 ?>
